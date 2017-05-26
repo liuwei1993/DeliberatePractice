0e563bdacb15c62fd24769167baa007b5d4f82c6
Simon Liu <simonliu@tcl.com>
2017-05-26 19:29:22 +0800

    add calc all combination
    Signed-off-by: Simon Liu <simonliu@tcl.com>

--- src/com/simon/string/permutation/AllPermutation.java
+++ src/com/simon/string/permutation/AllPermutation.java
@@ -12,50 +12,72 @@ public class AllPermutation {
 
     @Test
     public void testCalcAllPermutation() {
-        calcAllPermutation("123456".toCharArray(),0,5);
+        calcAllPermutation("123".toCharArray(),0);
     }
 
 
-    static void calcAllPermutation(char[] chars, int start, int end) {
-        if(start == end) {
+    static void calcAllPermutation(char[] chars, int pointer) {
+        int length = chars.length;
+        if(pointer == length - 1) {
             System.out.println(Arrays.toString(chars));
         } else {
-            for (int i = start; i <= end; i++) {
-                swap(chars, start, i);
-                calcAllPermutation(chars, start + 1, end);
-                swap(chars, start, i);
+            for (int i = pointer; i < length; i++) {
+                swap(chars, pointer, i);
+                calcAllPermutation(chars, pointer + 1);
+                swap(chars, pointer, i);
             }
         }
 
     }
 
     static void swap(char[] chars, int from, int to) {
+        if(from == to) return;
         char tmp = chars[to];
         chars[to] = chars[from];
         chars[from] = tmp;
     }
 
 
-    static void calcAllPermutationNotSimple(char[] result, char[] chars, int count, int pointer) {
+    /*
+    * 打印出字符串chars中所有元素的非简单全排列
+    * 类似进制计算，元素个数决定几进制，也可想象n个转轮组合在一起，也就是ofo单车的密码锁的结构
+    * */
+    static void calcAllPermutationNotSimple(char[] result, char[] chars, int pointer) {
+        int count = chars.length;
         if(count == pointer) {
             System.out.println(Arrays.toString(result));
         } else {
             for (int i = 0; i < count; i++) {
                 result[pointer] = chars[i];
-                calcAllPermutationNotSimple(result, chars, count, pointer+1);
+                calcAllPermutationNotSimple(result, chars, pointer+1);
             }
         }
     }
 
-    //TODO 未完成
     @Test
     public void testCalcAllPermutationNotSimple() {
-        char[] chars = "abcd".toCharArray();
-        for (int count = 0; count < chars.length; count++) {
-            char[] result = new char[count];
-            calcAllPermutationNotSimple(result, chars, count, 0);
-        }
-
+        char[] chars = "abc".toCharArray();
+        char[] result = new char[chars.length];
+        calcAllPermutationNotSimple(result, chars, 0);
     }
+
+    static void calcAllCombination(char[] chars) {
+        int length = chars.length;
+        int combinationCount = (int) Math.pow(length,2);
+        for (int count = 0; count < combinationCount; count++) {
+            for (int i = 0; i < chars.length; i++) {
+                if((count & (1 << i)) != 0) {
+                    System.out.print(chars[i]);
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    @Test
+    public void testCalcAllCombination() {
+        calcAllCombination("abc".toCharArray());
+    }
+
 
 }
